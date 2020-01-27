753a2113f55858c5573961d6e476708ea301864f
Vikram Seshadri <34171350+vikramsesh@users.noreply.github.com>
2020-01-27 15:12:30 -0500

    Update liveview.py

--- liveview.py
+++ liveview.py
@@ -1,33 +1,38 @@
+import os
 from omxplayer.player import OMXPlayer
 import subprocess
 
-#GUI
+# GUI
 from PyQt5 import QtWidgets, uic, QtCore
 from PyQt5.QtCore import Qt
 
-cmd="""kill $(pgrep omxplayer)"""
+cmd = """kill $(pgrep omxplayer) & pkill -f gphoto2"""
 
-import os
 
 class MainWindow(QtWidgets.QMainWindow):
-    
+
     def __init__(self):
         self.do_init = QtCore.QEvent.registerEventType()
-        QtWidgets.QMainWindow.__init__(self)        
+        QtWidgets.QMainWindow.__init__(self)
         super(MainWindow, self).__init__()
         self.ui = uic.loadUi("uiFiles/liveviewclose.ui", self)
-        self.move(840,385)
-        
+        self.move(840, 385)
+
         # Disable title bar
         self.setWindowFlags(QtCore.Qt.CustomizeWindowHint)
-        
+
         self.pushButton_close.clicked.connect(self.closeOMX)
-            
+
     def closeOMX(self):
-        process = subprocess.Popen(cmd, shell = True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        process = subprocess.Popen(
+            cmd,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
         output, error = process.communicate()
         print(output)
         QtWidgets.qApp.closeAllWindows()
+
 
 if __name__ == '__main__':
     import sys
