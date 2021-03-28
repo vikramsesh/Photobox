3397968c858fc72fcb076e5d8a593052288ee434
vikramsesh <vikramseshadri95@gmail.com>
2021-03-27 23:14:41 -0400

    removed email capabilities

--- main.py
+++ main.py
@@ -23,29 +23,17 @@ Filename check and fixed LiveView crashing issue
 
 """
 
-import sys
 import os
 import logging
 import time
-import datetime
 import gphoto2 as gp
-from validate_email import validate_email
 from omxplayer.player import OMXPlayer
 import subprocess
 import re
 import liveview
 
-from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
-# email
-import smtplib
-import ssl
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
-import socket
 
 # GUI
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
@@ -57,13 +45,11 @@ from uiFiles import cameracontrols
 # variables
 flag = 0
 foodflag = 0
-email_flag = 0
 filename = ""
 pic_dir = ""
 name_dir = ""
 new_dir = ""
-receiver_email = [""]
-bashCommand = """gphoto2 --capture-movie --stdout> fifo.mjpg & omxplayer -o hdmi --win "840 525 1680 1050" --alpha 230 fifo.mjpg & python /home/pi/Desktop/Photobox/liveview.py"""
+bashCommand = """gphoto2 --capture-movie --stdout> fifo.mjpg & omxplayer -o hdmi --win "840 525 1680 1050 --alpha 230 fifo.mjpg & python /home/pi/Desktop/Photobox/liveview.py"""
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -72,7 +58,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.do_init = QtCore.QEvent.registerEventType()
         QtWidgets.QMainWindow.__init__(self)
         super(MainWindow, self).__init__()
-        self.ui = uic.loadUi("/home/pi/Desktop/Photobox/uiFiles/cameracontrols.ui", self)
+        uiFiles = os.path.join(parent_dir, "uiFiles")
+        mainUI = os.path.join(uiFiles, "cameracontrols.ui")
+        self.ui = uic.loadUi(mainUI, self)
         self.move(0, 40)
 
         # defer full initialisation (slow operation) until gui is visible
@@ -88,18 +76,18 @@ class MainWindow(QtWidgets.QMainWindow):
         self.pushButton_quit.clicked.connect(QtWidgets.qApp.closeAllWindows)
 
         # Toggle Other lineEdit based on foodload
-        self.ui.comboBox_foodload.activated[str].connect(self.other_click)
+        self.ui.comboBox_foodload.activated[str].connect(self.other_food)
 
         # Connect to Camera button - Checks if camera is connected
-        self.pushButton_camerastatus.clicked.connect(self.camerasummary)
+        self.pushButton_camerastatus.clicked.connect(self.camera_summary)
 
         # Live View
-        self.pushButton_liveView.clicked.connect(self.liveView)
+        self.pushButton_liveView.clicked.connect(self.live_view)
 
         # Capture button
         self.pushButton_capture.clicked.connect(self.initialise)
 
-    def imagesettings(self):
+    def image_settings(self):
 
         # adjust camera configuratiuon
         cfg = self.camera.get_config()
@@ -157,9 +145,8 @@ class MainWindow(QtWidgets.QMainWindow):
         iso_cfg.set_value('100')
         aperture_cfg.set_value('4.5')
         shutter_cfg.set_value('1/40')
-        
+
         self.camera.set_config(cfg)
-        
 
     def camera_capture(self):
         global filename, pic_dir, name_dir, new_dir
@@ -167,59 +154,50 @@ class MainWindow(QtWidgets.QMainWindow):
         new_message = ""
 
         try:
-            print("1")
             self.camera.init()
-            
-            print("2")
+            logging.info("Camera initialized")
+
             # Camera parameters
-            self.imagesettings()
-            
-            print("3")
-            
+            self.image_settings()
+            logging.info("Camera parameters set")
+
             # Checking if duplicate image exists
             i = 1
-            new_dir = pic_dir + "/" + filename + "_" + str(i) + '.jpg'
+            new_dir = os.path.join(pic_dir, filename + "_" + str(i) + '.jpg')
             while os.path.exists(new_dir):
                 i += 1
-                new_dir = pic_dir + "/" + filename + "_" + str(i) + '.jpg'
+                new_dir = os.path.join(pic_dir, filename + "_" + str(i) + '.jpg')
 
             path = self.camera.capture(gp.GP_CAPTURE_IMAGE)
             camera_file = self.camera.file_get(
                 path.folder, path.name, gp.GP_FILE_TYPE_NORMAL)
-          
+
             # Save Image
             camera_file.save(new_dir)
-            
+
             # free camera
             self.camera.exit()
-            
-            if email_flag == 0:
-                try:
-                    new_message = self.SendMail()
-                except BaseException:
-                    new_message = "Unable to send email\n➊Check email ID\n➋Check Network connection.\nPlease use external drive to copy images\n"
 
             if os.path.exists(new_dir):
                 self.textEdit_message.setText(
                     str(new_message) + "Picture Captured and Stored in‣ \n" + pic_dir)
-                
-                #Display Image    
-                img=mpimg.imread(new_dir)
-                imgplot = plt.imshow(img)
+
+                # Display Image
+                img = mpimg.imread(new_dir)
+                plt.imshow(img)
                 plt.show()
 
-                # ~ Image.open(new_dir).show()
-                
                 QMessageBox.information(
                     None,
                     "Capture successful",
                     str(new_message) +
                     "Picture Captured and Stored in‣ \n" +
                     pic_dir)
-                
+
 
             else:
                 self.textEdit_message.setText("Image not Captured")
+                logging.error("Image not captured")
                 QMessageBox.critical(
                     None,
                     "Image not captured",
@@ -227,11 +205,13 @@ class MainWindow(QtWidgets.QMainWindow):
 
         except BaseException:
             self.textEdit_message.setText(
-                "➊Check if camera is powered on.\n➋Remove lens cover.\n➌Turn off the camera and turn it back on.\n➍Replace Battery.\n➎Place an object in front of the camera.")
+                "➊Check if camera is powered on.\n➋Remove lens cover.\n➌Turn off the camera and turn it back on.\n"
+                "➍Replace Battery.\n➎Place an object in front of the camera.\n ❻Free up storage")
             QMessageBox.critical(
                 None,
                 "Lens Error",
-                "➊Check if camera is powered on.\n➋Remove lens cover.\n➌Turn off the camera and turn it back on.\n➍Replace Battery.\n➎Place an object in front of the camera.")
+                "➊Check if camera is powered on.\n➋Remove lens cover.\n➌Turn off the camera and turn it back on.\n"
+                "➍Replace Battery.\n➎Place an object in front of the camera.\n ❻Free up storage")
 
         filename = ""
 
@@ -243,36 +223,25 @@ class MainWindow(QtWidgets.QMainWindow):
             flag = 1
 
     def gui_setup(self):
-        global flag, foodflag, filename, pic_dir, name_dir, receiver_email, email_flag
+        global flag, foodflag, filename, pic_dir, name_dir
 
         filename = ""
         flag = 0
         foodflag = 0
-        email_flag = 0
         messageString = ""
 
         # User input data
         # name*
         self.name = str(self.ui.lineEdit_name.text())
-        
+
         if self.name == "" or self.name.lower() == "your name here":
             messageString = "Enter Name\n"
             flag = 1
 
         else:
             flag = 0
-        
-        self.name = re.sub(r"([^\w])","",self.name)
-        
-        # email
-        self.email = str(self.ui.lineEdit_email.text())
-        if self.email == "" or self.email == "Email1, Email2 ...":
-            email_flag = 1
-        else:
-            email_flag = 0
-            receiver_email = [
-                x.strip() for x in str(
-                    self.ui.lineEdit_email.text()).split(",")]
+
+        self.name = re.sub(r"([^\w])", "", self.name)
 
         foodcount = 0
 
@@ -318,12 +287,11 @@ class MainWindow(QtWidgets.QMainWindow):
             QMessageBox.critical(None, "*Important info", messageString)
 
         if flag == 0 and foodflag == 0:
-            name_dir = data_dir + "/" + str(self.name)
+            name_dir = os.path.join(data_dir, str(self.name))
             if not os.path.exists(name_dir):
                 os.makedirs(name_dir)
 
-            pic_dir = name_dir + "/" + \
-                str(self.foodload) + " " + str(time.strftime("%m.%d.%Y"))
+            pic_dir = os.path.join(name_dir, str(self.foodload) + " " + str(time.strftime("%m.%d.%Y")))
             if not os.path.exists(pic_dir):
                 os.makedirs(pic_dir)
 
@@ -339,9 +307,9 @@ class MainWindow(QtWidgets.QMainWindow):
                 filename += "_" + str(self.filenameextra)
 
             filename += "_" + str(time.strftime("%m.%d.%Y"))
-            filename = re.sub(r"([^\w])","_",filename)
+            filename = re.sub(r"([^\w])", "_", filename)
 
-    def other_click(self):
+    def other_food(self):
         if str(self.ui.comboBox_foodload.currentText()) == "Other":
             self.label_other.setEnabled(True)
             self.lineEdit_other.setEnabled(True)
@@ -349,24 +317,24 @@ class MainWindow(QtWidgets.QMainWindow):
             self.label_other.setEnabled(False)
             self.lineEdit_other.setEnabled(False)
 
-    def liveView(self):
+    def live_view(self):
         global bashCommand
         makefile = """ mkfifo fifo.mjpg """
-        fifofile_dir = os.path.abspath(os.curdir) + "/fifo.mjpg"
+        fifofile_dir = os.path.join(os.path.abspath(os.curdir), "fifo.mjpg")
 
         try:
             self.textEdit_message.setText(
                 "If LiveView is not visible:\n‣Check if camera is powered on.\n‣Unplug and replug the Camera USB.")
             if not os.path.exists(fifofile_dir):
                 time.sleep(1)
-                process1 = subprocess.Popen(
+                process = subprocess.Popen(
                     makefile,
                     shell=True,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.STDOUT,
                     close_fds=False)
 
-            process = subprocess.Popen(
+            subprocess.Popen(
                 bashCommand,
                 shell=True,
                 stdout=subprocess.PIPE,
@@ -374,7 +342,9 @@ class MainWindow(QtWidgets.QMainWindow):
                 close_fds=False)
             try:
                 output, error = process.communicate()
-            except BaseException:
+                logging.info('liveView output:{}, error:{}'.format(output, error))
+            except Exception:
+                logging.exception("Exception occurred", exc_info=True)
                 self.textEdit_message.setText(
                     "‣Check Camera Connection.\n➊Close the application.\n➋Unplug and re plug the camera USB cable.")
                 QMessageBox.critical(
@@ -382,7 +352,8 @@ class MainWindow(QtWidgets.QMainWindow):
                     "LiveView Error",
                     "‣Check Camera Connection.\n➊Close the application.\n➋Unplug and re plug the camera USB cable.")
 
-        except BaseException:
+        except Exception:
+            logging.exception("Exception occurred", exc_info=True)
             self.textEdit_message.setText(
                 "‣Check Camera Connection.\n➊Close the application.\n➋Unplug and re plug the camera USB cable.")
             QMessageBox.critical(
@@ -390,7 +361,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 "LiveView Error",
                 "‣Check Camera Connection.\n➊Close the application.\n➋Unplug and re plug the camera USB cable.")
 
-    def camerasummary(self):
+    def camera_summary(self):
         self.camera = gp.Camera()
         try:
             self.camera.init()
@@ -431,8 +402,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
         except BaseException:
             self.textEdit_camstatus.setText("‣Could not detect any camera")
+            logging.error("Could not detect camera")
             self.textEdit_message.setText(
-                "No Camera detected - Check the following:\n➊Camera is ON.\n➋Battery is present and charged.\n➌USB is connected.")
+                "No Camera detected - Check the following:\n➊Camera is ON.\n➋Battery is present and charged.\n"
+                "➌USB is connected.")
             QMessageBox.warning(
                 None,
                 "No Camera detected",
@@ -457,7 +430,6 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Labels
         self.label_name.setEnabled(toggle_status)
-        #self.label_email.setEnabled(toggle_status)
         self.label_foodload.setEnabled(toggle_status)
         self.label_sku.setEnabled(toggle_status)
         self.label_software.setEnabled(toggle_status)
@@ -468,96 +440,31 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Line Edits
         self.lineEdit_name.setEnabled(toggle_status)
-        #self.lineEdit_email.setEnabled(toggle_status)
         self.lineEdit_sku.setEnabled(toggle_status)
         self.lineEdit_software.setEnabled(toggle_status)
         self.lineEdit_build.setEnabled(toggle_status)
         self.lineEdit_FilenameExtra.setEnabled(toggle_status)
 
-    def SendMail(self):
-
-        global receiver_email, new_dir
-        n_message = " "
-
-        # Change to your own account information
-        # Account Information
-        # Email to send from. (MUST BE GMAIL)
-        gmail_user = 'sharkninjatestfixtures@gmail.com'
-        gmail_password = '$h@rkNinj@'  # Gmail password.
-        smtpserver = smtplib.SMTP('smtp.gmail.com', 587)  # Server to use.
-
-        smtpserver.ehlo()  # Says 'hello' to the server
-        smtpserver.starttls()  # Start TLS encryption
-        smtpserver.ehlo()
-        smtpserver. login(gmail_user, gmail_password)  # Log in to server
-
-        img_data = open(new_dir, 'rb').read()
-        msg = MIMEMultipart()
-        msg['Subject'] = "Photobox Image: " + \
-            str(self.foodload) + " " + str(time.strftime("%m.%d.%Y"))
-        msg['From'] = 'sharkninjatestfixtures@gmail.com'
-        # ~ msg['To'] = receiver_email
-
-        text = MIMEText("Name: " +
-                        str(self.name) +
-                        "\n" +
-                        "Foodload: " +
-                        str(self.foodload) +
-                        "\n" +
-                        "Time: " +
-                        str(time.strftime("%r")) +
-                        "\n" +
-                        "Date: " +
-                        str(time.strftime("%m.%d.%Y (%A)")) +
-                        "\n\n" +
-                        "Regards,\nNinja Test Team")
-
-        msg.attach(text)
-
-        img_data = open(new_dir, 'rb').read()
-        image = MIMEImage(img_data, name=os.path.basename(new_dir))
-        msg.attach(image)
-
-        cnt = 0
-        # Sends the message
-        for i in range(0, len(receiver_email)):
-            is_valid = validate_email(receiver_email[i], check_mx=True)
-            is_valid2 = validate_email(receiver_email[i], verify=True)
-
-            if is_valid and is_valid2:
-                smtpserver.sendmail(
-                    gmail_user, receiver_email[i], msg.as_string())
-                cnt = cnt + 1  # Counts if all the email IDs are valid
-
-        if cnt < len(receiver_email):
-            n_message = "Check EmailID\n"
-        else:
-            n_message = "Email Successfull\n"
-
-        # Closes the smtp server.
-        smtpserver.quit()
-        receiver_email = [""]
-        return n_message
-
 
 if __name__ == "__main__":
     import sys
+
     if (sys.flags.interactive != 1):
         parent_dir = os.path.dirname(__file__)  # Current directory
-        data_dir = os.path.join(os.path.abspath(os.curdir) , "Pictures")  # RAW data folder
-        log_dir = os.path.join(os.path.abspath(os.curdir) , "Logs")  # Log files folder
-        
+        data_dir = os.path.join(os.path.abspath(os.curdir), "Pictures")  # Pictures folder
+        log_dir = os.path.join(os.path.abspath(os.curdir), "Logs")  # Log files folder
+
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
 
+        # Logging
         log_file = os.path.join(log_dir, "log" + str(time.strftime("-%m-%d-%Y--%I-%M-%S %p")) + ".log")
-        
-        logging.basicConfig(
-            filename=log_file,
-            format='%(asctime)s %(message)s',
-            level=logging.DEBUG)
+
+        logging.basicConfig(filename=log_file, filemode='w', level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+        logging.getLogger().addHandler(logging.StreamHandler())
 
         callback_obj = gp.check_result(gp.use_python_logging())
         app = QtWidgets.QApplication(sys.argv)
